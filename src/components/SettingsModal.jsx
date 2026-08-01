af72415739a03e30f19ecf8b9c99f8c7318d1729
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-01 14:45:41 -0400

    nest styling

--- src/components/SettingsModal.jsx
+++ src/components/SettingsModal.jsx
@@ -7,44 +7,41 @@ export default function SettingsModal({ isVisible, preferences }) {
         <header>
           <h2>Settings</h2>
         </header>
-        <main>
-          <section>
-            <h3>Time (Minutes)</h3>
-            <div className="form-row">
-              <div>
-                <label htmlFor="pomodoro-time">Pomodoro</label>
-                <input
-                  type="number"
-                  id="pomodoro-time"
-                  name="pomodoro-time"
-                  value={preferences.times.pomodoro}
-                />
-              </div>
-              <div>
-                <label htmlFor="short-break-time">Short break</label>
-                <input
-                  type="number"
-                  id="short-break-time"
-                  name="short-break-time"
-                  value={preferences.times.shortBreak}
-                />
-              </div>
-              <div>
-                <label htmlFor="long-break-time">Long break</label>
-                <input
-                  type="number"
-                  id="long-break-time"
-                  name="long-break-time"
-                  value={preferences.times.longBreak}
-                />
-              </div>
+        <section>
+          <h3>Time (Minutes)</h3>
+          <div className="time-group">
+            <div>
+              <label htmlFor="pomodoro-time">Pomodoro</label>
+              <input
+                type="number"
+                id="pomodoro-time"
+                name="pomodoro-time"
+                value={preferences.times.pomodoro}
+              />
             </div>
-          </section>
-
-          <button type="submit" className="btn">
-            Apply
-          </button>
-        </main>
+            <div>
+              <label htmlFor="short-break-time">Short break</label>
+              <input
+                type="number"
+                id="short-break-time"
+                name="short-break-time"
+                value={preferences.times.shortBreak}
+              />
+            </div>
+            <div>
+              <label htmlFor="long-break-time">Long break</label>
+              <input
+                type="number"
+                id="long-break-time"
+                name="long-break-time"
+                value={preferences.times.longBreak}
+              />
+            </div>
+          </div>
+        </section>
+        <button type="submit" className="btn">
+          Apply
+        </button>
       </form>
     </div>
   );
