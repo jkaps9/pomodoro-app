eaa404664187cf822e47bb74a4560ca2404bca13
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-01 22:28:40 -0400

    add test script

---
+++ src/components/SettingsButton.test.jsx
@@ -0,0 +1,3 @@
+import { render, screen } from "@testing-library/react";
+import SettingsButton from "./SettingsButton";
+import { describe, it, expect } from "vitest"; // If globals are not enabled
